254304bda9783a1784b24d87072cab8ab634da21
Kai Wang <kaiwsv@gmail.com>
2023-12-04 13:18:38 -0800

    change controller so move amount can be a float

--- Assignments/Core/Src/controller.c
+++ Assignments/Core/Src/controller.c
@@ -9,7 +9,7 @@
 /*
  * We recommend you implement this function so that move(1) will move your rat 1 cell forward.
  */
-void move(int8_t n) {
+void move(float n) {
 	/*
 	 * For assignment 3.1: Don't worry about implementing this yet
 	 * For assignment 3.2: This function should set the distance and angle goals appropriately for PID (hint: using the setGoal functions in pid.c)
@@ -19,8 +19,7 @@ void move(int8_t n) {
 	 *
 	 * You should also call resetPID before exiting this function so your rat is ready for the next instruction.
 	 */
-	//TODO set distance number - completely wrong rn
-	setPIDGoalD(n*5000); // estimating that one square is 5000 encoder counts long - not at the lab
+	setPIDGoalD(n*650);
 
 	while (!PIDdone()) {
 	}
@@ -28,10 +27,10 @@ void move(int8_t n) {
 }
 
 /*
- * We recommend you implement this function so that turn(1) turns your rat 90 degrees in your positive rotation
+ * We recommend you implement this function so that turn(1) turns your rat 90 degrees in your positive rotation (left)
  * direction and turn(-1) turns the other way.
  */
-void turn(int8_t n) {
+void turn(float n) {
 	/*
 	 * For assignment 3.1: Don't worry about implementing this yet
 	 * For assignment 3.2: This function should set the distance and angle goals appropriately for PID (hint: using the setGoal functions in pid.c)
@@ -42,7 +41,7 @@ void turn(int8_t n) {
 	 * You should also call resetPID before exiting this function so your rat is ready for the next instruction.
 	 */
 	//445 difference in encoder count ~= 90 degree turn
-	setPIDGoalA(n*500); //TODO tune degrees slightly
+	setPIDGoalA(n*620);
 	while(!PIDdone()) { // wait until done
 	}
 
